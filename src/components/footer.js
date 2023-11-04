2149c99240c815bc05793e38ddad4906ab3ad50f
Jon Stockdill <jon.stockdill@gmail.com>
2023-11-04 13:36:32 +0000

    Powered by greenwoodjs logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,8 @@ class FooterComponent extends HTMLElement {
       return `
   
         <footer>
-          <h4>Cloud RI &copy;${year} | Powered by <a href="https://www.greenwoodjs.io">greenwoodjs</a></h4>
+          <h4>Cloud RI &copy;${year} | 
+          Powered by <a href="https://www.greenwoodjs.io"><img class="h-5 inline" src="https://www.greenwoodjs.io/assets/greenwood-logo-og.png" /> greenwoodjs</a></h4>
         </footer>
       `;
     }
